cbb5f3c91e93f0c0b4146525e60f007dee5c3ea7
sanek95k <sanek95k@mail.ru>
2014-04-27 11:23:52 +0300

    A simple mesh class

---
+++ lib/graphics/wackyMesh.d
@@ -0,0 +1,82 @@
+import derelict.assimp3.assimp;
+import derelict.opengl3.gl3;
+
+/**
+ *  The class represents simple meshes
+ */
+class WackyMesh
+{
+public:
+
+    this(float[] vertices, uint[] indices, float[] textureCoordinates)
+    {
+        this.vertices = vertices;
+        this.indices = indices;
+        this.textureCoordinates = textureCoordinates;
+
+        generateBuffers();
+    }
+
+    ~this()
+    {
+        glDeleteBuffers(buffers.length, buffers.ptr);
+        glDeleteVertexArrays(1, &VAO);
+    }
+
+    auto render()
+    {
+        glBindVertexArray(VAO);
+        glDrawElementsBaseVertex(GL_TRIANGLES, indices.length, GL_UNSIGNED_INT, cast (void*) indices.ptr, 0);
+        glBindVertexArray(0);
+
+    }
+
+private:
+    /*
+     *  Normals' support is not implemented yet
+     *  Vector 2f[] normals
+     */
+    float[] vertices;
+    float[] textureCoordinates;
+    uint[] indices;
+
+    enum
+    {
+        POSITION = 0,
+        TEXTURE_COORDINATES = 1,
+        INDICES = 2,
+    }
+
+    uint buffers[3];
+
+    /**
+     *  Vertex array object
+     */
+    uint VAO;
+
+    /**
+     *  Creates VAO
+     */
+    auto generateBuffers()
+    {
+
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+        glGenBuffers(buffers.length, buffers.ptr);
+
+        glBindBuffer(GL_ARRAY_BUFFER, buffers[POSITION]);
+        glBufferData(GL_ARRAY_BUFFER, float.sizeof * vertices.length, vertices.ptr, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * float.sizeof, cast (const void*) 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, buffers[TEXTURE_COORDINATES]);
+        glBufferData(GL_ARRAY_BUFFER, float.sizeof * textureCoordinates.length, textureCoordinates.ptr, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * float.sizeof, cast (const void*) 0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, buffers[INDICES]);
+        glBufferData(GL_ARRAY_BUFFER, uint.sizeof * indices.length, indices.ptr, GL_STATIC_DRAW);
+
+        glBindVertexArray(0);
+    }
+}
