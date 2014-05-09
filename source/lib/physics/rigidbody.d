8e4aade3519fd83636d793516657a6b12cd4b249
artemjackson <artemjackson95@gmail.com>
2014-05-09 21:25:08 +0300

    more flexability is added to rigidbody

--- source/lib/physics/rigidbody.d
+++ source/lib/physics/rigidbody.d
@@ -13,87 +13,149 @@ public
  */
 class RigidBody
 {
-public:
-    this() pure nothrow @safe
+public: 
+    /**
+     *  Constructor with default parametrs.
+     *  by default Vector3f, Quaternionf have zero values
+     */
+    this(float mass = 0.0f, Vector3f position = Vector3f(), Quaternionf orientation = Quaternionf(), Geometry geometry = null) pure nothrow @safe
+    in
     {
-        mass = 1.0f;
-        invMass = 1.0f;
-        /*  Default zero initialization for
-         *
-         *  - position
-         *  - position;
-         *  - linearVelocity;
-         *  - linearAcceleration;
-         *  - forceAccumulator;
-         *  - orientation;
-         *  - angularVelocity;
-         *  - angularAcceleration;
-         *  - torqueAccumulator;
-         */
-        inertia = Matrix3x3f.identity;
-        invInertia = Matrix3x3f.identity;
-
-        geometry = null;
+        assert(mass > float.epsilon, "RigidBody(float mass = 1.0f, Vector3f position = Vector3f(), Vector3f orientation = Vector3f(), Geometry geometry = null): Invalid mass vass value. Mass of rigib body should by more then float.epsilon");
     }
-
+    body
+    {
+        _mass = mass;
+        _invMass = 1.0f / mass;
+        _position = position;
+        _orientation = orientation;
+        
+        /*  Default zero initialization for:
+         *
+         *  _linearVelocity;
+         *  _linearAcceleration;
+         *  _forceAccumulator;
+         *
+         *  _angularVelocity;
+         *  _angularAcceleration;
+         *  _torqueAccumulator;
+         */
+        _geometry = geometry;
+        
+        if(_geometry !is null)
+            setGeometry(_geometry);
+    }
+   
     void integrateForces(float dt) pure nothrow @safe
     {
-        linearAcceleration = forceAccumulator * invMass;
-        linearVelocity += linearAcceleration * dt;
+        _linearAcceleration = _forceAccumulator * _invMass;
+        _linearVelocity += _linearAcceleration * dt;
 
 
-        angularAcceleration = inertia * torqueAccumulator;
-        angularVelocity += angularAcceleration * dt;
+        _angularAcceleration = _inertia * _torqueAccumulator;
+        _angularVelocity += _angularAcceleration * dt;
     }
 
     void integrateVelocities(float dt) pure nothrow @safe
     {
-        if (linearVelocity.length < float.epsilon)
-            linearVelocity = Vector3f(0.0f, 0.0f, 0.0f);
-        if (angularVelocity.length < float.epsilon)
-            angularVelocity = Vector3f(0.0f, 0.0f, 0.0f);
+        if (_linearVelocity.length < float.epsilon)
+            _linearVelocity = Vector3f();   // by default it's a Vector3f(0, 0 , 0);
+        if (_angularVelocity.length < float.epsilon)
+            _angularVelocity = Vector3f();  // by default it's a Vector3f(0, 0 , 0);
 
-        position += linearVelocity * dt;
-        orientation += 0.5f * Quaternionf(angularVelocity, 0.0f) * orientation * dt;
+        _position += _linearVelocity * dt;
+        _orientation += 0.5f * Quaternionf(_angularVelocity, 0f) * _orientation * dt;
         // orientation.normalize();
     }
 
     void setGeometry(Geometry geometry) pure nothrow @safe
     {
-        this.geometry = geometry;
+        _geometry = geometry;
 
-        inertia = geometry.inertiaTensor(mass);
+        _inertia = geometry.inertiaTensor(_mass);
         //  invInertia = inertia.inverse;
     }
 
     void applyForce(Vector3f force) pure nothrow @safe
     {
-        forceAccumulator += force;
+        _forceAccumulator += force;
     }
 
     void applyTorque(Vector3f torque) pure nothrow @safe
     {
-        torqueAccumulator += torque;
+        _torqueAccumulator += torque;
     }
-
+    
+    /**
+     *  Gettrs
+     */
+    @property float mass() pure nothrow @safe
+    {
+        return _mass;
+    }
+    
+    @property float invMass() pure nothrow @safe
+    {
+        return _invMass;
+    }
+    
+    @property Vector3f position() pure nothrow @safe
+    {
+        return _position;
+    }
+    
+    @property Vector3f linearVelocity() pure nothrow @safe
+    {
+        return _linearVelocity;
+    }
+    
+    @property Vector3f linearAcceleration() pure nothrow @safe
+    {
+        return _linearAcceleration;
+    }
+    
+    @property Matrix3x3f inertia() pure nothrow @safe
+    {
+        return _inertia; 
+    }
+    
+    @property Matrix3x3f invInertia() pure nothrow @safe
+    {
+        return _invInertia; 
+    }
+    
+    @property Quaternionf orientation() pure nothrow @safe
+    {
+        return _orientation; 
+    }
+    
+    @property Vector3f angularVelocity() pure nothrow @safe
+    {
+        return _angularVelocity; 
+    }
+    
+    @property Vector3f angularAcceleration() pure nothrow @safe
+    {
+        return _angularAcceleration; 
+    }
+    
 private:
+    float _mass;
+    float _invMass;
 
-    float mass;
-    float invMass;
-
-    /*  By default Vector, Quaternion and Matrix are created with zero values   */
-    Vector3f position;
-    Vector3f linearVelocity;
-    Vector3f linearAcceleration;
-    Vector3f forceAccumulator;
+    /*  By default Vector3f, Quaternionf and Matrix3x3f are created with zero values   */
+    Vector3f _position;
+    Vector3f _linearVelocity;
+    Vector3f _linearAcceleration;
+    Vector3f _forceAccumulator;
 
 
-    Matrix3x3f inertia;
-    Matrix3x3f invInertia;
-    Quaternionf orientation;
-    Vector3f angularVelocity;
-    Vector3f angularAcceleration;
-    Vector3f torqueAccumulator;
+    Matrix3x3f _inertia;
+    Matrix3x3f _invInertia;
+    Quaternionf _orientation;
+    Vector3f _angularVelocity;
+    Vector3f _angularAcceleration;
+    Vector3f _torqueAccumulator;
 
-    Geometry geometry;
+    Geometry _geometry;
 }
