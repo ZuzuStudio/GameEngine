cdc848068e520b6bfd98727b15d48b392ad80284
artemjackson <artemjackson95@gmail.com>
2014-05-15 21:22:09 +0300

    update CollisionTest.d

--- applications/PhysicsTests/CollisionTest.d
+++ applications/PhysicsTests/CollisionTest.d
@@ -2,6 +2,7 @@ import std.stdio;
 import lib.math.vector;
 
 import lib.physics.physicsworld;
+import lib.geometry.sphere;
 
 /**
  *  Two spheres collision test
@@ -9,30 +10,32 @@ import lib.physics.physicsworld;
 void main()
 {
     PhysicsWorld world = new PhysicsWorld(0.0f);
-    world.add(new RigidBody(    // id = 0
+    world.addDynamicBody(new RigidBody(    // id = 0
                              1.0f,  // mass
                              Vector3f(0,0,0),   // position
-                             Quaternion3f(0,0,0,0), // orientation
+                             Quaternionf(0,0,0,0), // orientation
                              new Sphere(Vector3f(0,0,0), 4.0f)  // geometry
                             )
               );
 
-    world.add(new RigidBody(    // id = 1
+    world.addDynamicBody(new RigidBody(    // id = 1
                              1.0f,  // mass
                              Vector3f(16,0,0),   // position
-                             Quaternion3f(0,0,0,0), // orientation
+                             Quaternionf(0,0,0,0), // orientation
                              new Sphere(Vector3f(0,0,0), 4.0f)  // geometry
                             )
               );
 
-    world.getDynamicBody[0].applyForce(2,0,0);
-    world.getDynamicBody[0].applyTorque(2,0,0);
+    world.getDynamicBody(0).applyForce(Vector3f(2,0,0));
+    world.getDynamicBody(0).applyTorque(Vector3f(2,0,0));
 
-    world.getDynamicBody[1].applyForce(-2,0,0);
-    world.getDynamicBody[1].applyTorque(-2,0,0);
+    world.getDynamicBody(1).applyForce(Vector3f(-2,0,0));
+    world.getDynamicBody(1).applyTorque(Vector3f(-2,0,0));
 
-    forech(i; 0..1_000_000)
+    foreach(i; 0..1_000){
         world.update(1 / 60.0f);
-    
-    return 0;
+
+        writeln(world.getDynamicBody(0).position);
+        writeln(world.getDynamicBody(1).position);
+    }
 }
