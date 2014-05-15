8a06c3033e605da857547a37ee4e93d1f208c4c2
artemjackson <artemjackson95@gmail.com>
2014-05-16 00:42:34 +0300

    update

--- applications/PhysicsTests/CollisionTest.d
+++ applications/PhysicsTests/CollisionTest.d
@@ -14,7 +14,7 @@ void main()
     world.addDynamicBody(new RigidBody(    // id = 0
                              1.0f,  // mass
                              Vector3f(0f, 0f, 0f),   // position
-                             Quaternionf(0f, 0f, 0f, 0f), // orientation
+                             Quaternionf(0f, 0f, 0f, 1f), // orientation
                              new Sphere(Vector3f(0,0,0), 1.0f)  // geometry
                             )
               );
@@ -22,22 +22,28 @@ void main()
     world.addDynamicBody(new RigidBody(    // id = 1
                              1.0f,  // mass
                              Vector3f(50,0,0),   // position
-                             Quaternionf(0,0,0,0), // orientation
+                             Quaternionf(0,0,0,1), // orientation
                              new Sphere(Vector3f(50,0,0), 1.0f)  // geometry
                             )
               );
 
     world.getDynamicBody(0).applyForce(Vector3f(2000,0,0));
-    world.getDynamicBody(0).applyTorque(Vector3f(0,0,0));
+    world.getDynamicBody(0).applyTorque(Vector3f(2000,0,0));
 
     world.getDynamicBody(1).applyForce(Vector3f(-2000,0,0));
-    world.getDynamicBody(1).applyTorque(Vector3f(0,0,0));
+    world.getDynamicBody(1).applyTorque(Vector3f(2000,0,0));
 
-    foreach(i; 0..100){
+    foreach(i; 0..10){
+        writeln("b1 angv:", world.getDynamicBody(0).angularVelocity);
+
+        writeln("b1 pos:", world.getDynamicBody(0).position);
+        writeln("b1 orinet:", world.getDynamicBody(0).orientation);
+
+        writeln("b2 pos:", world.getDynamicBody(1).position);
+        writeln("b2 orient:", world.getDynamicBody(1).orientation);
+        writeln();
         world.update(1 / 60.0f);
 
-        writeln("b1:", world.getDynamicBody(0).position);
-        writeln("b2:", world.getDynamicBody(1).position);
-        writeln();
+
     }
 }
